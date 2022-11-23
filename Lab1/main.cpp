fd030dd58d2a7d6acc8ad86d29f13bc523aaece4
Tarodictrl <vudi600@gmail.com>
2022-11-23 15:10:00 +0700

    Edit lab1

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -3,9 +3,9 @@
  * @author Tarodictrl
  * @version 0.1
  * @date 2022-11-23
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #include "iostream"
@@ -15,10 +15,9 @@
 
 using namespace std;
 
-
 int main()
 {
-    DynamicArray* array = new DynamicArray();
+    DynamicArray *array = new DynamicArray();
 
     InitDynamicArray(array);
     cout << "0. Close program.\n"
@@ -29,7 +28,8 @@ int main()
             "5. Linear element search.\n"
             "6. Binary element search.\n"
             "7. Recreate the array.\n"
-            "8. Output an array \n" << endl;
+            "8. Output an array \n"
+         << endl;
     cout << endl;
 
     while (true)
@@ -38,37 +38,37 @@ int main()
         int number = CheckForDigit();
         switch (number)
         {
-            case 0:
-                delete[] array->Array;
-                delete array;
-                return -1;
-            case 1:
-                AddElement(array);
-                break;
-            case 2:
-                RemoveElement(array);
-                break;
-            case 3:
-                InsertElement(array);
-                break;
-            case 4:
-                SortArray(array);
-                break;
-            case 5:
-                LinearSearchElement(array);
-                break;
-            case 6:
-                BinarySearchElement(array);
-                break;
-            case 7:
-                RecreateDynamicArray(array);
-                break;
-            case 8:
-                PrintDynamicArray(array);
-                break;
-            default:
-                cout << "Incorrect number!" << endl;
-                break;
+        case 0:
+            delete[] array->Array;
+            delete array;
+            return -1;
+        case 1:
+            AddElement(array);
+            break;
+        case 2:
+            RemoveElement(array);
+            break;
+        case 3:
+            InsertElement(array);
+            break;
+        case 4:
+            SortArray(array);
+            break;
+        case 5:
+            LinearSearchElement(array);
+            break;
+        case 6:
+            BinarySearchElement(array);
+            break;
+        case 7:
+            RecreateDynamicArray(array);
+            break;
+        case 8:
+            PrintDynamicArray(array);
+            break;
+        default:
+            cout << "Incorrect number!" << endl;
+            break;
         }
     }
 }
